be76ed41dcbd28fa95abbf11d9b0d283f8f090e5
cfx.404 <C-F-4@users.noreply.github.com>
2017-02-23 14:13:27 +0530

    Update and rename Voting to Voting.java

--- hr.gs/web-dev-st3/Voting.java
+++ hr.gs/web-dev-st3/Voting.java
@@ -1,14 +1,17 @@
 /*
-It's election time in Hackerland. M citizens casted votes to candidates. To win the election, the candidate must have the highest 
-number of votes.
+It's election time in Hackerland. M citizens casted votes to candidates. To win the election, the candidate must 
+have the highest number of votes.
 Condition: There will always be a winner.
-If there are multiple candidates with the highest vote, then that candidate shall win whose name come in the last when sorted in 
-ascending alphabetically.
+If there are multiple candidates with the highest vote, then that candidate shall win whose name come in the last 
+when sorted in ascending alphabetically.
+
 Input format:
 First line consists of a number N which is an Integer, that is the total number of candidates.
 The next N lines contains a list of N votes to candidates.
+
 Constraints:
 0 < N <= 10000
+
 Sample Test Case:
 10
 Mary
@@ -20,5 +23,8 @@ James
 Anna
 Michael
 Jack
+Michael
+
+Output:
 Michael
 */
